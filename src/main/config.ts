ce61751b4848acd8eef4aaa3abc1ad8c9cbe91dc
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-01-15 22:45:48 -0300

    fix: add new const to env

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-export const PORT = process.env.PORT || 8001
+export const PORT = Number(process.env.PORT) || 8001;
+export const HASH_SALT = Number(process.env.HASH_SALT);
