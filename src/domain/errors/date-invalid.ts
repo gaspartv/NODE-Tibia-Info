be46e5bc1cebd96c359e6b2fe1e298d5820633fe
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-01-15 22:45:20 -0300

    fix: delete useless files

--- src/domain/errors/date-invalid.ts
+++
@@ -1,9 +0,0 @@
-import AppError from "./app-error";
-
-class DateInvalidError extends AppError {
-  constructor() {
-    super("date is not a valid format", 400);
-  }
-}
-
-export default DateInvalidError;
--- src/domain/errors/id-invalid.ts
+++
@@ -1,9 +0,0 @@
-import AppError from "./app-error";
-
-class IdInvalidError extends AppError {
-  constructor() {
-    super("id not a valid uuid", 400);
-  }
-}
-
-export default IdInvalidError;
